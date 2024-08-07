8fffe44dd23e97f3bc651e80703e49c421639b85
wkanu <wkanu@wustl.edu>
2024-08-07 16:10:02 -0500

    fifth commit

--- app/page.js
+++ app/page.js
@@ -1,17 +1,41 @@
 import Image from "next/image";
 import Link from "next/link";
 
+const styles = {
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: '100vh',
+    backgroundColor: 'white',
+    textAlign: 'center', // Center text
+  },
+  heading: {
+    fontSize: '36px',
+    marginBottom: '20px',
+  },
+  button: {
+    backgroundColor: '#FFD580', // Assuming this is your button color
+    color: 'black',
+    border: 'none',
+    padding: '10px 20px',
+    borderRadius: '5px',
+    cursor: 'pointer',
+    fontSize: '16px',
+    textDecoration: 'none', // Ensures no underline for the Link
+    display: 'inline-block', // To ensure the button styles are applied correctly
+  },
+}
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main style={styles.container}>
       <div>
-        <h1>
-          Quiz app
-        </h1>
-        <Link href = '/quiz'>
-          <button>Start Quiz</button>
+        <h1 style={styles.heading}>Article 26 Website</h1>
+        <Link href="/login" passHref>
+          <button style={styles.button}>Login</button>
         </Link>
-
       </div>
     </main>
   );
